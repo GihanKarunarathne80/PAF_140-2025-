d1dad9a3362451934d99bd8242203efe4ea2d4c2
AmadhiICIEOS <amadhi@icieos.com>
2025-04-12 16:55:00 +0530

    postshare new

--- paf-frontend-2025-v2-main/src/pages/CookingPostsPage.jsx
+++ paf-frontend-2025-v2-main/src/pages/CookingPostsPage.jsx
@@ -22,7 +22,7 @@ const CookingPostsPage = () => {
       const data = (await cookingPostApi.getAllPosts()).reverse();
       setPosts(data);
     } catch (error) {
-      toast.error("Failed to fetch posts");
+      toast.error("Failed to fetch posts!");
       console.error(error);
     } finally {
       setLoading(false);
@@ -54,7 +54,7 @@ const CookingPostsPage = () => {
   return (
     <div className="container w-[800px] mx-auto px-4 py-8">
       <div className="flex justify-between items-center mb-8">
-        <h1 className="text-3xl font-bold text-gray-800">Skill sharing posts</h1>
+        <h1 className="text-3xl font-bold text-gray-800">Skill Sharing Posts</h1>
         <button
           onClick={openCreateModal}
           className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition"
